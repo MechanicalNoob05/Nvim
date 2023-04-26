410dc4961b4c0daf92b30ecb87a80c55a4c56919
Mecahnical noob <you@example.com>
2023-04-27 01:42:50 +0530

    some new remaps and adjustments

--- after/plugin/tabline.lua
+++ after/plugin/tabline.lua
@@ -2,8 +2,8 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<A-PageUp>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-PageDown>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-h>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-l>', '<Cmd>BufferNext<CR>', opts)
 
 -- Close Buffer
 map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
