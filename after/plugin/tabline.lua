b07a6d65d13c3c85588c46690ffe181c4ff4aa74
Tejas mayekar <tejasmayekar13@gmail.com>
2023-06-09 12:17:36 +0530

    redo in kickstart nvim

--- after/plugin/tabline.lua
+++
@@ -1,9 +0,0 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Move to previous/next
-map('n', '<A-h>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-l>', '<Cmd>BufferNext<CR>', opts)
-
--- Close Buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
--- lua/tejas/init.lua
+++
@@ -1,3 +0,0 @@
-require("tejas.remap")
-require("tejas.set")
-
